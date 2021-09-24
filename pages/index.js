e5248a47c76e52aa2c4d132cbaa3b1cd62b1df91
RobertJewell <rjewellaudio@gmail.com>
2021-09-24 14:39:52 +0200

    move components and store out of pages dir

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { useStore } from "./store/store";
-import Game from "./components/Game";
-import ResultScreen from "./components/ResultScreen";
+import { useStore } from "../store/store";
+import Game from "../components/Game";
+import ResultScreen from "../components/ResultScreen";
 
 export default function Home() {
 	// const [gameStage, setGameStage] = useState("setup");
