2551eca3c5a4a9a3deaac545bb1d071c9e21b0ee
RobertJewell <rjewellaudio@gmail.com>
2021-09-24 15:15:31 +0200

    clean up index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { useStore } from "../store/store";
 import Game from "../components/Game";
 import ResultScreen from "../components/ResultScreen";
 
 export default function Home() {
-	// const [gameStage, setGameStage] = useState("setup");
-
 	const gameStage = useStore((state) => state.gameStage);
-	const updateGameStage = useStore((state) => state.updateGameStage);
 
 	return (
 		<div>
